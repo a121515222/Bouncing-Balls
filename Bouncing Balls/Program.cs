edb7d070c703903777e2298f37908d3543248f1e
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-12-12 00:53:37 +0800

    First update

---
+++ Bouncing Balls/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Bouncing_Balls
+{
+    public static class BouncingBall
+    {
+        public static int BallBouncing(float ballStartHeight, float bounceRate, float windowsHeight)
+        {
+            var icorrectParameter = -1;
+            
+            if (ballStartHeight > 0 && bounceRate is > 0 and < 1 && ballStartHeight - windowsHeight > 0)
+            {
+                var result = 1;
+                var ballBounce = ballStartHeight * bounceRate;
+                while (ballBounce >= windowsHeight)
+                {
+                    result += 2;
+                    ballBounce *= bounceRate;
+                    Console.WriteLine(ballBounce);
+                }
+                return result;
+            }
+            else return icorrectParameter;
+        }
+    }
+}
---
+++ Bouncing Balls/Run.cs
@@ -0,0 +1,9 @@
+﻿using Bouncing_Balls;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(BouncingBall.BallBouncing(3.0f, 0.66f,1.5f));
+    }
+}
